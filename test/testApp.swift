bb3b1e1f0c48240d17aecec9b360e432a41ed46d
Nicola De Filippo <nicola@nicoladefilippo.it>
2022-02-14 08:29:50 +0100

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by nicola on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
